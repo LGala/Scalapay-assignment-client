bdf29ff73855eec574dc2a07e1d700cebdba22e2
LGala <galafassilorenzo@gmail.com>
2022-02-08 04:17:40 +0100

    add configuration/setup files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "../src/reducers/rootReducer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { ErrorPage } from "./views/ErrorPage";
+import CreateOrder from "./views/CreateOrder";
+import ChooseItems from "./views/ChooseItems";
+import { TopBar } from "./components/TopBar";
+
+import "./styles/global.css";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const storex = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+render(
+  <Provider store={storex}>
+    <BrowserRouter>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<ChooseItems />} />
+        <Route path="/create-order" element={<CreateOrder />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
