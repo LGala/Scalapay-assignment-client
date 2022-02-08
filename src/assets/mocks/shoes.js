111cbada2da1b082f89636f949d55cbbf2eb5e9c
LGala <galafassilorenzo@gmail.com>
2022-02-08 04:11:53 +0100

    add assets

---
+++ src/assets/mocks/shoes.js
@@ -0,0 +1,62 @@
+export const itemsData = [
+  {
+    gtin: "UPC",
+    price: { amount: "80", currency: "EUR" },
+    name: "Air Max Plus",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "0",
+    brand: "Nike",
+    imageName: "airmax_plus.jpeg"
+  },
+  {
+    gtin: "UPC",
+    price: { amount: "100", currency: "EUR" },
+    name: "Air Max Deluxe",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "1",
+    brand: "Nike",
+    imageName: "airmax_deluxe.jpeg"
+  },
+  {
+    gtin: "UPC",
+    price: { amount: "95", currency: "EUR" },
+    name: "Air Max 720",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "2",
+    brand: "Nike",
+    imageName: "airmax_720.jpeg"
+  },
+  {
+    gtin: "UPC",
+    price: { amount: "90", currency: "EUR" },
+    name: "Air Max 270",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "3",
+    brand: "Nike",
+    imageName: "airmax_270.jpeg"
+  },
+  {
+    gtin: "UPC",
+    price: { amount: "105", currency: "EUR" },
+    name: "Vapormax Flyknit",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "4",
+    brand: "Nike",
+    imageName: "airmax_vapormax_flyknit.jpeg"
+  },
+  {
+    gtin: "UPC",
+    price: { amount: "97", currency: "EUR" },
+    name: "Air Max 90",
+    category: "Sport",
+    subcategory: ["Shoes"],
+    sku: "5",
+    brand: "Nike",
+    imageName: "airmax_90.jpeg"
+  }
+];
